eabc0221f037f267d2ff92d9e6b36e02f0235ceb
Phuong Tieu <tieuduyphuong93@gmail.com>
2019-03-13 14:59:08 +0700

    ngay 13 thang 3

--- zalo-app/src/pages/home/home.ts
+++ zalo-app/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+username: string='';
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
-
+  presentAlert(title: string, message: string) {
+    let alert = this.alertCtrl.create({
+      title: title ,
+      subTitle:message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  Login(){
+    if(/^[a-zA-Z0-9]+$/.test(this.username)){
+      //tot
+    }else{
+      this.presentAlert('Lỗi','ID không hợp lệ');
+    }
+  }
 }
